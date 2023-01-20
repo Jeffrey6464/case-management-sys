13772e38f6bb1af7611f99133dbc9a6d5403eb42
jeffrey6464 <71198970+Jeffrey6464@users.noreply.github.com>
2023-01-20 13:38:55 -0600

    Update temp.rs

--- src/temp.rs
+++ src/temp.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, PartialEq)]
 struct Test {
   data: String,
   count: i32,
@@ -7,6 +8,7 @@ impl Test {
   fn send (self: &self, data: String) -> Option<i32> {
     thread::send(self.data);
     self.count = self.count + 1;
+    println!("data: {}, count: {}", self.data.clone(), self.count.clone())
     self.count
   }
 }
